166c5106c13bbf072103aff9794ab74c8599fc79
gknikolic <gknikolic@elfak.rs>
2021-10-01 00:00:55 +0200

    delete old project

--- app/src/main/java/com/example/findpet/RepositoryEventListeners/UsersListEventListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.findpet.RepositoryEventListeners;
-
-public interface UsersListEventListener {
-    void OnUsersListUpdated();
-}
--- app/src/main/java/com/example/findpet/data_models/Pet.java
+++
@@ -1,4 +0,0 @@
-package com.example.findpet.data_models;
-
-public class Pet {
-}
