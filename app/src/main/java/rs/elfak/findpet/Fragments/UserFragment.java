4f8a488d79590bdd8a23031e2f9936910a8d2749
zBalanovic <z.balanovic@elfak.rs>
2021-10-05 20:42:45 +0200

    Added camera capture for profile picture

--- app/src/main/java/rs/elfak/findpet/Fragments/UserFragment.java
+++ app/src/main/java/rs/elfak/findpet/Fragments/UserFragment.java
@@ -2,11 +2,17 @@ package rs.elfak.findpet.Fragments;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,15 +20,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import java.io.File;
 import java.io.IOException;
 
 import rs.elfak.findpet.Helpers.Constants;
@@ -74,23 +83,72 @@ public class UserFragment extends Fragment {
                 {
                     requestPermissions(
                             new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                            2000);
+                            Constants.STORAGE_PERMISSION_CODE);
                 }
                 else {
-                    startGallery();
-//                    pickImage();
+                    startDialog();
                 }
             }
         });
     }
 
-    private void startGallery() {
-        Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        cameraIntent.setType("image/*");
-        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(cameraIntent, Constants.STORAGE_REQUEST_CODE);
-        }
+    private void startDialog() {
+        AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(
+                getActivity());
+        myAlertDialog.setTitle("Upload Pictures Option");
+        myAlertDialog.setMessage("How do you want to set your picture?");
+
+        myAlertDialog.setPositiveButton("Gallery",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        if(ActivityCompat.checkSelfPermission(getActivity(),
+                                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                        {
+                            requestPermissions(
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    Constants.STORAGE_PERMISSION_CODE);
+                        }
+                        else{
+                            Intent pictureActionIntent = null;
+
+                            pictureActionIntent = new Intent(
+                                    Intent.ACTION_PICK,
+                                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(
+                                    pictureActionIntent,
+                                    Constants.STORAGE_REQUEST_CODE);
+
+                        }
+
+                    }
+                });
+
+        myAlertDialog.setNegativeButton("Camera",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        if(ActivityCompat.checkSelfPermission(getActivity(),
+                                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                        {
+                            requestPermissions(
+                                    new String[]{Manifest.permission.CAMERA},
+                                    Constants.CAMERA_PERMISSION_CODE);
+                        }
+                        else{
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(intent, Constants.CAMERA_REQUEST_CODE);
+                        }
+                    }
+                });
+        myAlertDialog.show();
     }
+
+//    private void startGallery() {
+//        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        galleryIntent.setType("image/*");
+//        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//            startActivityForResult(galleryIntent, Constants.STORAGE_REQUEST_CODE);
+//        }
+//    }
 
     private void pickImage() {
         CropImage.activity()
@@ -103,19 +161,26 @@ public class UserFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         getActivity();
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == 1000) {
-                Uri returnUri = data.getData();
-                Bitmap bitmapImage = null;
-                try {
-                    bitmapImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), returnUri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        if (requestCode == Constants.STORAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri returnUri = data.getData();
+            Bitmap bitmapImage = null;
+            try {
+                bitmapImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), returnUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (bitmapImage != null){
+                Bitmap.createScaledBitmap(bitmapImage, 400, 400, true);
                 UsersData.getInstance().changeUserProfilePicture(bitmapImage, getContext());
                 UsersData.getInstance().getCurrentLoggedUser().profilePicture=bitmapImage;
                 profileImageImageView.setImageBitmap(bitmapImage);
             }
+        }
+        else if(requestCode == Constants.CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            UsersData.getInstance().changeUserProfilePicture(photo, getContext());
+            UsersData.getInstance().getCurrentLoggedUser().profilePicture=photo;
+            profileImageImageView.setImageBitmap(photo);
         }
     }
 
--- app/src/main/java/rs/elfak/findpet/Helpers/Constants.java
+++ app/src/main/java/rs/elfak/findpet/Helpers/Constants.java
@@ -5,5 +5,8 @@ public class Constants {
     public static final String FREINDS_KEY = "Freinds";
     public static final String PET_KEY = "Pet";
     public static final int STORAGE_REQUEST_CODE = 1000;
+    public static final int CAMERA_REQUEST_CODE = 1001;
     public static final int LOCATION_REQUEST_CODE = 2000;
+    public static final int STORAGE_PERMISSION_CODE = 3000;
+    public static final int CAMERA_PERMISSION_CODE = 3001;
 }
